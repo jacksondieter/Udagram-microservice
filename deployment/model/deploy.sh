27a8552b960615fea673d5618989223b6e7e4728
Dieter Jackson <jacksondieter@gmail.com>
2021-09-21 20:36:10 +0200

    deploy: Fix autoscaling

--- deployment/model/deploy.sh
+++ deployment/model/deploy.sh
@@ -16,8 +16,11 @@ kubectl apply -f reverseproxy-service.yaml
 #Expose External IP
 kubectl expose deployment frontend --type=LoadBalancer --name=publicfrontend
 kubectl expose deployment reverseproxy --type=LoadBalancer --name=publicreverseproxy
+
 #Autoscaling
-kubectl autoscale deployment reverseproxy --cpu-percent=70 --min=3 --max=5
-kubectl autoscale deployment backend-user --cpu-percent=70 --min=3 --max=5
-kubectl autoscale deployment backend-feed --cpu-percent=70 --min=3 --max=5
-kubectl autoscale deployment frontend --cpu-percent=70 --min=3 --max=5
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl get deployment metrics-server -n kube-system
+kubectl autoscale deployment reverseproxy --cpu-percent=50 --min=1 --max=5
+kubectl autoscale deployment backend-user --cpu-percent=50 --min=1 --max=5
+kubectl autoscale deployment backend-feed --cpu-percent=50 --min=1 --max=5
+kubectl autoscale deployment frontend --cpu-percent=50 --min=1 --max=5
